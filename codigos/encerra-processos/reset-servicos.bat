2da092bc3edc0a56b0553432ed01b92e16074832
Gabriel-Moya <45153493+Gabriel-Moya@users.noreply.github.com>
2021-09-01 14:19:59 -0400

    Create reset-services.bat
    Batch file for restart services lojamix

---
+++ codigos/encerra-processos/reset-servicos.bat
@@ -0,0 +1,11 @@
+NET STOP LojamixRemotingService
+NET START LojamixRemotingService
+
+NET STOP LojamixProcessadorService
+NET START LojamixProcessadorService
+
+NET STOP LojamixPdvService
+NET START LojamixPdvService
+
+NET STOP LojamixConnectionService
+NET START LojamixConnectionService
